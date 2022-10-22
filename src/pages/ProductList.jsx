ce7eb5814e9c7df3e6b01cb3c632823a8feb63c4
kholostyak17 <kholostyak17@gmail.com>
2022-10-22 18:47:18 +0200

    Create ProductList page layout

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Search from "../components/Search";
+
+const SortPicker = () => {
+  return (
+    <select name="select">
+      <option value="value1">Value 1</option>
+      <option value="value2" selected>
+        Value 2
+      </option>
+      <option value="value3">Value 3</option>
+    </select>
+  );
+};
 
 const ProductList = () => {
-  return <div className="list-page">esta es la galeria</div>;
+  return (
+    <div className="list-page">
+      <h1>Smartphones</h1>
+      <div>
+        <div>
+          <SortPicker />
+          <Search />
+        </div>
+        <div className="list-elements">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
