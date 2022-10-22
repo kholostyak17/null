645e974ad0b6564db7271f81963da743db5f18ef
kholostyak17 <kholostyak17@gmail.com>
2022-10-22 19:46:34 +0200

    Integrate list with real data

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Search from "../components/Search";
+
+const API_URL = "https://front-test-api.herokuapp.com/api";
 
 const SortPicker = () => {
   return (
@@ -14,6 +17,23 @@ const SortPicker = () => {
 };
 
 const ProductList = () => {
+  const [list, setList] = useState([]);
+
+  const getList = async () => {
+    const response = await fetch(`${API_URL}/product`);
+    const list = await response.json();
+    try {
+      console.log(response, list, "TRY");
+      setList(list);
+    } catch {
+      console.log(response, list, "CATCHH");
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
   return (
     <div className="list-page">
       <h1>Smartphones</h1>
@@ -23,10 +43,19 @@ const ProductList = () => {
           <Search />
         </div>
         <div className="list-elements">
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
+          {Boolean(list.length) &&
+            list.map((elem) => {
+              return (
+                <Link to={`/${elem.id}`} key={elem.id}>
+                  <div className="item">
+                    <img src={elem.imgUrl} alt="smartphone picture" />
+                    <h3>{elem.brand}</h3>
+                    <p>{elem.model}</p>
+                    <p>{elem.price}€</p>
+                  </div>
+                </Link>
+              );
+            })}
         </div>
       </div>
     </div>
