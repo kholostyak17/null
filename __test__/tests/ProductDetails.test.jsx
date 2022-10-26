314905dcc1c8a341d486457d3601c6ed42292713
kholostyak17 <kholostyak17@gmail.com>
2022-10-27 00:59:59 +0200

    Complete final tests

--- __test__/tests/ProductDetails.test.jsx
+++ __test__/tests/ProductDetails.test.jsx
@@ -1,13 +1,24 @@
+import mockAxios from "jest-mock-axios";
 import React from "react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./common";
 import ProductDetails from "../../src/pages/ProductDetails";
+import { productDetailsResponse } from "./mockedResponses";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest.fn().mockReturnValue({ id: "TyVJrrYOXZ7tUIp5CmQpf" }),
+}));
 
 describe("ProductDetails component", () => {
-  it("Renders successfully", () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  it("Renders successfully without data", () => {
     const component = render(
       <BrowserRouter>
         <Provider store={store}>
@@ -16,5 +27,24 @@ describe("ProductDetails component", () => {
       </BrowserRouter>
     );
     expect(component.getByTestId("details-page")).toBeInTheDocument();
+  });
+
+  it("Renders successfully with mock data", async () => {
+    mockAxios.get.mockResolvedValueOnce(productDetailsResponse);
+
+    const component = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <ProductDetails />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      expect(component.getByTestId("details-page")).toBeInTheDocument();
+      expect(component.getByTestId("image")).toBeInTheDocument();
+      expect(component.getByTestId("description")).toBeInTheDocument();
+      expect(component.getByTestId("actions")).toBeInTheDocument();
+    });
   });
 });
--- __test__/tests/ProductList.test.jsx
+++ __test__/tests/ProductList.test.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./common";
 import ProductList from "../../src/pages/ProductList";
+import { productListResponse } from "./mockedResponses";
+import mockAxios from "jest-mock-axios";
+
+jest.mock("axios");
 
 describe("ProductList component", () => {
-  const component = render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <ProductList />
-      </Provider>
-    </BrowserRouter>
-  );
+  afterEach(() => {
+    mockAxios.reset();
+  });
 
   it("Renders successfully", () => {
+    mockAxios.get.mockResolvedValueOnce(productListResponse);
+    const component = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <ProductList />
+        </Provider>
+      </BrowserRouter>
+    );
     expect(component.getByTestId("list-page")).toBeInTheDocument();
   });
 });
